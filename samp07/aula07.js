fd9289b793b23af1de8ce1d7352d23e3fca5787d
Diego de Lima Zapora <diego.limazapora@gmail.com>
2025-05-27 11:55:00 -0300

    faf

---
+++ samp07/aula07.js
@@ -0,0 +1,18 @@
+let num = 10
+let res = (num%2 ? "Impar" : "Par")
+console.log(res)
+
+let num1 = 10
+let num2 = 5
+if (num1 > num2) {
+    console.log("Verdadeiro")
+} else {
+    console.log("Falso")
+}
+
+let st="A"
+if (st === "A") {
+    console.log("Ativo")
+} else {
+    console.log("Inativo")
+}
---
+++ samp08/aula08.js
@@ -0,0 +1,9 @@
+let num1 = 20
+let samp = "Andre Matos"
+let v3 = num1===samp
+let v4 = {nome: "Diego"}
+
+console.log(`Valor: ${num1} Tipo: ${typeof(num1)}`)
+console.log(samp)
+console.log(v3)
+console.log(v4)
