e1787ca6e578ba1726d555e326a1a2162eac29bd
Diego de Lima Zapora <diego.limazapora@gmail.com>
2025-06-05 14:47:33 -0300

    ghghg

--- samp20/aula20.js
+++ samp20/aula20.js
@@ -10,3 +10,17 @@ const soma=(...valores) => {
 }
 
 console.log(soma(15,5))
+
+const nome = (...nomes) => {
+    const andre = (viper) => {
+        let res = 0
+        for (v of viper) {
+            res = res + v
+        }
+        return res
+    }
+
+    return andre(nomes)
+}
+
+console.log(nome(2025-2019))
