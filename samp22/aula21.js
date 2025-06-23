cd2fb85466d8a3dc4712e29263a94097534b05d8
Diego de Lima Zapora <diego.limazapora@gmail.com>
2025-06-23 07:54:50 -0300

    gf

--- samp22/aula21.js
+++ samp22/aula21.js
@@ -3,21 +3,19 @@ oi.map((el) => {
     return el.length
 })
 
-console.log(oi)
+console.log(oi)*/
 
-const albuns = ["Time to be Free", "Holy Land", "Mentalize", "Ritual", "Reason"]
-albuns.map((el) => {
+const sas = ["Time to be Free", "Holy Land", "Mentalize", "Ritual", "Reason"]
+const samp = sas.map((el) => {
     return el.toUpperCase()
 })
 
-console.log(albuns)*/
+console.log(samp)
 
 const numeros = [1, 2, 3]
-const dobrado = numeros.map((el => {
+const dobrado = numeros.map((el) => {
     return el*2
-}))
-
-console.log(numeros)
+})
 console.log(dobrado)
 
 const albuns = ["Mentalize", "Holy Land", "SAMP"]
@@ -25,5 +23,4 @@ const format = albuns.map((el, i) => {
     return `${i + 1}. ${el}`
 })
 
-console.log(albuns)
 console.log(format)
