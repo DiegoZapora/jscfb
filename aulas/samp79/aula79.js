0037c00ba1ff26459ecbd8c03a373ba85246adef
Diego de Lima Zapora <diego.limazapora@gmail.com>
2025-08-07 11:29:05 -0300

    asgagsg

--- aulas/samp79/aula79.js
+++ aulas/samp79/aula79.js
@@ -1,10 +1,12 @@
 const btnE = document.getElementById("btnEsquerda")
 const btnD = document.getElementById("btnDireita")
-const btnP = document.getElementById("btnParar")
 const btnB = document.getElementById("btnBaixo")
 const btnC = document.getElementById("btnCima")
 const carro = document.getElementById("carro")
 const branco = document.getElementById("branco")
+const outro = document.getElementById("outro")
+const divTempo = document.getElementById("tempo")
+const score = document.getElementById("score")
 
 const inicio = () => {
     carro.style = "position: relative; left: 0px; width: 100px; top: 0px"
@@ -13,40 +15,84 @@ const inicio = () => {
     branco.style.top = "5px"
 
     tamMax = window.innerWidth - parseInt(carro.style.width)
-    tamMaxY = window.innerHeight - parseInt(carro.style.top)
+    tamMaxY = window.innerHeight - parseInt(carro.style.width)
 }
-
-let audio = new Audio("assets/carro (1).mp3")
-let freio = new Audio("assets/carro (2).mp3")
-let batida = new Audio("assets/carri.mp3")
 
 let anima = null
 let tamMax = null
 let tamMaxY = null
 
+const over = () => {
+    location.reload()
+}
+
+let tempoVivo = null
+
+const tempo = () => {
+    tempoVivo++
+    divTempo.innerHTML = `Score: ${tempoVivo}`
+    setTimeout(tempo, 5000000000)
+}
+
 const  mover = (dir) => {
+    tempo()
     if (dir > 0) {
         if (parseInt(carro.style.left) <= tamMax) {
             carro.style.left = parseInt(carro.style.left) + (10*dir) +"px"
             anima = setTimeout(mover, 20, dir)
+            if (parseInt(carro.style.left) > tamMax) {
+                clearTimeout(tempo())
+                carro.style.backgroundColor = "white"
+                outro.style.display = "flex"
+                score.innerHTML = `Score: ${tempoVivo+1}`
+                setTimeout(over, 1500)
+                btnE.disabled = true
+                btnD.disabled = true
+                btnC.disabled = true
+                btnB.disabled = true
+            }
         } else {
             clearTimeout(anima)
         }
-    }else if (dir < 0) {
+    } else if (dir < 0) {
         if (parseInt(carro.style.left) >= 0) {
             carro.style.left = parseInt(carro.style.left) + (10*dir) +"px"
             anima = setTimeout(mover, 20, dir)
+            if (parseInt(carro.style.left) < 0) {
+                clearTimeout(tempo())
+                carro.style.backgroundColor = "white"
+                outro.style.display = "flex"
+                score.innerHTML = `Score: ${tempoVivo+1}`
+                setTimeout(over, 1500)
+                btnE.disabled = true
+                btnD.disabled = true
+                btnC.disabled = true
+                btnB.disabled = true
+            }
         } else {
             clearTimeout(anima)
         }
     }
 }
 
+
 const moverY = (dirY) => {
+    tempo()
     if (dirY > 0) {
         if (parseInt(carro.style.top) <= tamMaxY) {
             carro.style.top = parseInt(carro.style.top) + (10*dirY) +"px"
-            anima = setTimeout(moverY, 20, dirY) 
+            anima = setTimeout(moverY, 20, dirY)
+            if (parseInt(carro.style.top) > tamMaxY) {
+                clearTimeout(tempo())
+                carro.style.backgroundColor = "white"
+                outro.style.display = "flex"
+                score.innerHTML = `Score: ${tempoVivo+1}`
+                setTimeout(over, 1500)
+                btnE.disabled = true
+                btnD.disabled = true
+                btnC.disabled = true
+                btnB.disabled = true
+            }
         } else {
             clearTimeout(anima)
         }
@@ -55,7 +101,18 @@ const moverY = (dirY) => {
     if (dirY < 0) {
         if (parseInt(carro.style.top) >= 0) {
             carro.style.top = parseInt(carro.style.top) + (10*dirY) +"px"
-            anima = setTimeout(moverY, 20, dirY) 
+            anima = setTimeout(moverY, 20, dirY)
+            if (parseInt(carro.style.top) < 0) {
+                clearTimeout(tempo())
+                carro.style.backgroundColor = "white"
+                outro.style.display = "flex"
+                score.innerHTML = `Score: ${tempoVivo+1}`
+                setTimeout(over, 1500)
+                btnE.disabled = true
+                btnD.disabled = true
+                btnC.disabled = true
+                btnB.disabled = true
+            }
         } else {
             clearTimeout(anima)
         }
@@ -83,8 +140,6 @@ window.addEventListener("keypress", (w) => {
 })
 
 btnD.addEventListener("click", () => {
-    freio.pause()
-    audio.play()
     clearTimeout(anima)
     mover(+1)
     branco.style.left = "68px"
@@ -96,8 +151,6 @@ btnD.addEventListener("click", () => {
 })
 
 btnE.addEventListener("click", () => {
-    freio.pause()
-    audio.play()
     clearTimeout(anima)
     mover(-1)
     branco.style.left = "10px"
@@ -108,32 +161,22 @@ btnE.addEventListener("click", () => {
     branco.style.height = "30px"
 })
 
-btnP.addEventListener("click", () => {
-    clearTimeout(anima)
-    audio.pause()
-    freio.play()
-})
-
 btnB.addEventListener("click", () => {
-    freio.pause()
-    audio.play()
     clearTimeout(anima)
     moverY(+1)
     carro.style.width = "45px"
     carro.style.height = "100px"
     branco.style.top = "70px"
-    branco.style.left = "7px"
+    branco.style.left = "5px"
     branco.style.width = "33px"
     branco.style.height = "20px"
 })
 
 btnC.addEventListener("click", () => {
-    freio.pause()
-    audio.play()
     clearTimeout(anima)
     moverY(-1)
     branco.style.top = "10px"
-    branco.style.left = "7px"
+    branco.style.left = "5px"
     carro.style.width = "45px"
     carro.style.height = "100px"
     branco.style.width = "33px"
@@ -147,5 +190,5 @@ window.addEventListener("resize", () => {
 })
 
 window.addEventListener("resize", () => {
-    tamMaxY = window.innerHeight - parseInt(carro.style.width)
+    tamMaxY = window.innerHeight - parseInt(carro.style.height)
 })
