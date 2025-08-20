543863aa0bb4e764fbe001431a1bfa54c16d38f7
Diego de Lima Zapora <diego.limazapora@gmail.com>
2025-08-20 11:25:44 -0300

    asfa

--- aulas/samp93/aula93.js
+++ aulas/samp93/aula93.js
@@ -1,3 +1,26 @@
 import {Login} from "./login.js";
 
-Login.login("andre", "matos")
+let logado = false
+let matlogado = null
+let nomelogado = null
+let acessologado = null
+let endPoint = "http://localhost:8080/"
+
+const login = (mat, pas) => {
+    endPoint += `?matricula=${mat}&senha=${pas}`;
+    fetch(endPoint)
+    .then(res => res.json())
+    .then(res => {
+        if (res) {
+            logado = true
+            matlogado = mat
+            nomelogado = res.nome
+            acessologado = res.acesso
+            console.log(res)
+        } else {
+            console.log("Usuario ou Senha não encontrados.")
+        }
+    })
+}
+
+login("andre", "matos")
